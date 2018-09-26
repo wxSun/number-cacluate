d408bb788fa51027921b2365b6bfc2c23f21c397
wxSun <18260712928@qq.com>
2018-09-26 14:28:39 +0800

    change the function prolog

--- qin_jiu_shao_duo_xiang_shi_ji_suan.c
+++ qin_jiu_shao_duo_xiang_shi_ji_suan.c
@@ -17,16 +17,30 @@
 */
 
 
-
 /******************************************************************************
-input : a[]  多项式系数
-        n    多项式阶数
-        x    待求值
-output :
-        多项式的值
+FUNCTION NAME :    double  dxsqz (double a[], int n,double x)
+
+ARGUMENTS :
+
+ARGUMENT    TYPE    I/O     DESCRIPTION   *
+-------     ----    ---     -----------   *
+a[]        double    I      多项式系数     *
+n          int       I      多项式阶数     *
+x          double    I      待求值      	*
+
+RETURN :
+
+ARGUMENT    TYPE    I/O     DESCRIPTION   *
+-------     ----    ---     -----------   *
+out         double   O      多项式的值     *
+
+
 license:  Hsinwang   2018.9.20
 *******************************************************************************/
-    double  dxsqz (double a[], int n,double x)
+
+
+
+	double  dxsqz (double a[], int n,double x)
  {
      int count=0;   // 计数器
      double out=a[n-1]; //  输出值，结果保存
