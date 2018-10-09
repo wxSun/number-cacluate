0d9a413b2a1b0055458621c918e9816c8d57a0fb
wxSun <18260712928@qq.com>
2018-10-09 22:20:32 +0800

    change

--- qu_jian_er_fen_fa/qjeff/qjeff_2.m
+++ qu_jian_er_fen_fa/qjeff/qjeff_2.m
@@ -1,12 +1,10 @@
 function [ output] = qjeff( left, right ,delta )
-% input
-%      (left,right)  ------ zuo_you_duan_dian
-%       delta        ______ jing_du
-% output
-%       output       _____  jin_si_ling_dian,die_dai_ci_shu
-%description
-%       er fen fa qiu jie fangcheng
-%       HsinWang
+%
+%
+%
+%
+%
+%
 middle=(left+right)*0.5;
 count=0;
 while abs(func(middle))>=delta
@@ -24,8 +22,12 @@ end
 
 
 function [ y] = func( x )
-%description 
-%           Main function 
-%           HsinWang
-y= 2*sin(x)-x ;
+%
+%
+%
+%
+%
+%
+%
+y= 1.0/3 * power(x,3) -power(x,2)+4 *x -4 ;
 end
