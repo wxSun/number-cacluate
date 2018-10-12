cc034077e9e228bbd45d4fc6796e014852bb91a5
wxSun <18260712928@qq.com>
2018-10-12 18:19:40 +0800

    change

--- hilbert_Matrix/hilbert.c
+++ hilbert_Matrix/hilbert.c
@@ -15,7 +15,7 @@ int main()
 int hilbert(double *p,int n)
 {
   int i=0; int j=0;//Iterator initialization
-  p= malloc ((n*n) *sizeof(double));//Dynamic memory opening
+  *p= malloc ((n*n) *sizeof(double));//Dynamic memory opening
   for(i=0;i<n;i++)
       for(j=0;j<n;j++)
         (*(P+i)+j)=1/i+j;ju//Matrix element assignment
