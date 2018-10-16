db440203da363e285d0d6708d90e76365bc5e373
wxSun <18260712928@qq.com>
2018-10-16 16:55:48 +0800

    change

--- hilbert_Matrix/hilbert.c
+++ hilbert_Matrix/hilbert.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main()
 {
+  void hilbert(double *p,int n);
+  double a[]={0};
 
+  hilbert(*a,4);
 
 
     return 0;
@@ -15,8 +18,11 @@ int main()
 void hilbert(double *p,int n)
 {
   int i=0; int j=0;//Iterator initialization
-  *p= malloc ((n*n) *sizeof(double));//Dynamic memory opening
   for(i=0;i<n;i++)
-      for(j=0;j<n;j++)
-        (*(P+i)+j)=1/i+j;//Matrix element assignment
+      for(j=0;j<n;j++){
+          (*(P+i)+j)=1/i+j;//Matrix element assignment
+          printf("%f\n",(*(P+i)+j) );
+      }
+return;
+
 }
