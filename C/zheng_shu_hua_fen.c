2e39ebd0a34e3d28f5bb30df21d102b86f617957
wxSun <18260712928@qq.com>
2018-12-04 16:22:26 +0800

    change

--- C/zheng_shu_hua_fen.c
+++
@@ -1,7 +0,0 @@
-
-
-
-long  int zshf(int n,int m)
-{
-    if ((n<1)||(m<1))       return 0;   if((n==1)||(m==1))      return 1;   if(n<m)    return zshf(n,n);     if(n==m)        return (zshf(n,m-1)+1);     return (zshf(n,m-1)+zshf(n-m,m));
-}
